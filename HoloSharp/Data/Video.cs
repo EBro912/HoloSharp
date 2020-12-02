7f18af9f4b0920d482f09fb20aab22fed99bbf36
Ethan <ebrogames@gmail.com>
2020-12-02 10:33:45 -0500

    Make Video objects nullable

--- HoloSharp/Data/Video.cs
+++ HoloSharp/Data/Video.cs
@@ -7,25 +7,25 @@ using System.Threading.Tasks;
 
 namespace HoloSharp.Data
 {
-    public sealed class Video : Stream
+    public sealed class Video : Stream 
     {
         /// <summary>
         /// Whether or not the video is uploaded and is not a stream.
         /// </summary>
         [JsonProperty("is_uploaded")]
-        public bool isUploaded { get; internal set; }
+        public bool? isUploaded { get; internal set; }
         
         /// <summary>
         /// The duration of the video, in seconds.
         /// </summary>
         [JsonProperty("duration_secs")]
-        public int Duration { get; internal set; }
+        public int? Duration { get; internal set; }
 
         /// <summary>
         /// Whether or not the video has closed captions.
         /// </summary>
         [JsonProperty("is_captioned")]
-        public bool isCaptioned { get; internal set; }
+        public bool? isCaptioned { get; internal set; }
 
         /// <summary>
         /// Any comments on the video which have a timestamp.
